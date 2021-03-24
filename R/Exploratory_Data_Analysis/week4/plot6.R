5ae61d6ad79622062be9dff53c95f33a4aa1540b
Vinicius Matias <45612047+matiasvinicius@users.noreply.github.com>
2021-03-23 22:36:02 -0300

    Create plot6.R

---
+++ R/Exploratory_Data_Analysis/week4/plot6.R
@@ -0,0 +1,51 @@
+library(plyr)
+library(dplyr)
+library(ggplot2)
+
+download_data <- function() {
+  if(!dir.exists("data")) {
+    message("Creating folder 'data'")
+    dir.create("data")
+    url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    
+    download.file(url, destfile = "data/emissions_data.zip")
+    unzip("data/emissions_data.zip")
+    file.rename(from = "Source_Classification_Code.rds", to = "data/SCC.rds")
+    file.rename(from = "summarySCC_PM25.rds", to = "data/summary_SCC.rds")
+    file.remove("data/emissions_data.zip")
+    
+    message("rds files downloaded in './data/'")
+  }
+}
+
+# How have emissions from motor vehicle sources changed from 
+# 1999-2008 in Baltimore City?
+download_data()
+
+if (!dir.exists("images")){
+  message("Creating folder 'images'")
+  dir.create("images")
+}
+
+message("Loading the rds files")
+NEI <- readRDS("./data/summary_SCC.rds") %>%
+  filter(type == "ON-ROAD",
+         fips %in% c("24510","06037")) %>%
+  select(Emissions, year, fips) %>%
+  group_by(fips, year) %>%
+  summarise(Emissions = sum(Emissions), .groups = "drop")
+
+NEI$fips <- mapvalues(NEI$fips, 
+                      from = c("24510","06037"),
+                      to = c("Baltimore City", "Los Angeles, California"))
+
+png('images/plot6.png')
+ggplot(data = NEI, aes(x = as.factor(year), y = Emissions)) + 
+  geom_bar(stat = "identity") +
+  facet_wrap(. ~ fips) +
+  ylab("Emissions (tons)") + 
+  xlab("Year") + 
+  ggtitle("Emissions from motor vehicle sources (1999-2008)") + 
+  theme(plot.title = element_text(hjust = 0.5))
+dev.off()
+message("plot6.png saved in 'images' directory")
